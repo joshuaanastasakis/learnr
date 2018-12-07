82188dab5c96bff74afae310a1bface69f8c371d
joshuaanastasakis <joshuaanastasakis@gmail.com>
2018-12-06 21:20:37 -0500

    initial commit

---
+++ build/precache-manifest.695895ea369bb0519dba3292bf853cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cba86be0d6d2e811b11",
+    "url": "/static/js/main.0cba86be.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "0cba86be0d6d2e811b11",
+    "url": "/static/css/main.74ef3d34.chunk.css"
+  },
+  {
+    "revision": "74ab5590bf9124fb14162e7610e96b6f",
+    "url": "/index.html"
+  }
+];
